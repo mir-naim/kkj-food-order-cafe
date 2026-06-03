0e153597447c4df95bb19b4240750410dfb7d9d0
Mir Naimur Rahman <112193631+mir-naim@users.noreply.github.com>
2026-06-03 23:35:03 +0600

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -231,13 +231,13 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
     httpOnly: true,
+    secure: true,
+    sameSite: "none",
   });
 
-  res.cookie("token", null, {
-  expires: new Date(Date.now()),
-  httpOnly: true,
-  secure: true,
-  sameSite: "none",
+  res.status(200).json({
+    success: true,
+    message: "Logged out successfully",
   });
 });
 

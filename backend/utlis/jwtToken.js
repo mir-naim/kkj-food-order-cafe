ab8ea9d88a9b2f6e089e82c8c748e36c701b3ac3
Mir Naimur Rahman <112193631+mir-naim@users.noreply.github.com>
2026-06-03 22:11:06 +0600

    Update jwtToken.js

--- backend/utlis/jwtToken.js
+++ backend/utlis/jwtToken.js
@@ -6,24 +6,27 @@
 
 
 //create and send token and save in the cookie
-const  sendToken = (user, statusCode, res) => {
+//create and send token and save in the cookie
+const sendToken = (user, statusCode, res) => {
 
     //create jwt token
     const token = user.getJwtToken();
 
     //options for cookie
-    const options ={
+    const options = {
         expires: new Date(
-            Date.now() +  process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true
-    }
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
+    };
 
     res.status(statusCode).cookie('token', token, options).json({
         success: true,
         token,
         user
-    })
-}
+    });
+};
 
 module.exports = sendToken;

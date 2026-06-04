cb1ba7d026664d50011316b147e860f46ffd4d7e
Mir Naimur Rahman <112193631+mir-naim@users.noreply.github.com>
2026-06-04 17:35:38 +0600

    Update ConfirmOrder.js

--- frontend/src/components/cart/ConfirmOrder.js
+++ frontend/src/components/cart/ConfirmOrder.js
@@ -72,7 +72,7 @@ const ConfirmOrder = () => {
 
       <div className="row d-flex justify-content-between">
         <div className="col-12 col-lg-8 mt-5 order-confirm">
-          <h4 className="mb-3">Shipping Info</h4>
+          <h4 className="mb-3">User Info</h4>
           <p>
             <b>Name:</b>
             {user && user.name}

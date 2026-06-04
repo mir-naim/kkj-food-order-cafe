97199fa3f1f401f079889beca5dc5551530bd7bc
Mir Naimur Rahman <112193631+mir-naim@users.noreply.github.com>
2026-06-04 10:18:47 +0600

    Update paymentController.js

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -11,19 +11,23 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 //Process stripe payments => /api/v1/payment/process
 
-exports.processPayment = catchAsyncErrors( async (req, res, next) => {
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: req.body.amount,
-        currency: 'myr',
-        metadata: {integration_check: 'accept_a_payment'}
-    });
+exports.processPayment = catchAsyncErrors(async (req, res, next) => {
 
-    res.status(200).json({
-        success: true,
-        client_secret: paymentIntent.client_secret
-    })
+  if (!req.body.amount) {
+    return next(new ErrorHandler("Amount is required", 400));
+  }
 
-})
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: req.body.amount,
+    currency: "myr",
+    metadata: { integration_check: "accept_a_payment" },
+  });
+
+  res.status(200).json({
+    success: true,
+    client_secret: paymentIntent.client_secret,
+  });
+});
 
 
 

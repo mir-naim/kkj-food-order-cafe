a08cac4a6faf5d36113b8d8f7246ae6b5d925504
Mir Naimur Rahman <112193631+mir-naim@users.noreply.github.com>
2026-06-04 10:33:23 +0600

    Update Payment.js

--- frontend/src/components/cart/Payment.js
+++ frontend/src/components/cart/Payment.js
@@ -23,6 +23,7 @@ import {
   CardCvcElement,
 } from "@stripe/react-stripe-js";
 import axios from "axios";
+import api from "../../utils/api";
 import { useAlert } from "react-alert";
 
 const options = {
@@ -68,7 +69,7 @@ const Payment = () => {
         },
       };
 
-      res = await axios.post("/api/v1/payment/process", paymentData, config);
+      res = await api.post("/api/v1/payment/process", paymentData, config);
 
       const clientSecret = res.data.client_secret;
       if (!stripe || !elements) {

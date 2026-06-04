618d6a28e58b0086862dd93e7300ec3bfd346494
Mir Naimur Rahman <112193631+mir-naim@users.noreply.github.com>
2026-06-04 17:41:12 +0600

    Update OrderDetails.js

--- frontend/src/components/order/OrderDetails.js
+++ frontend/src/components/order/OrderDetails.js
@@ -59,7 +59,7 @@ const OrderDetails = () => {
             <div className="col-12 col-lg-8 mt-5 order-details">
               <h1 className="my-5">Order # {order._id}</h1>
 
-              <h4 className="mb-4">Shipping Info</h4>
+              <h4 className="mb-4">User Info</h4>
               <p>
                 <b>Name:</b> {user && user.name}
               </p>
